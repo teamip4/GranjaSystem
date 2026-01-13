a87d7c9f720b5984f618055a7dfbc6c3283e4d15
João Paulo Sousa Menezes <joao.menezes21@Outlook.com>
2026-01-13 15:00:36 -0300

    Cria testes registro de usuários.

---
+++ GranjaSystemProject.Tests/Tests/TestInfrastructure/Builders/UserBuilder.cs
@@ -0,0 +1,56 @@
+﻿using GranjaSystemProject.Models.Users;
+using GranjaSystemProject.Tests.Tests.TestInfrastructure.Constants;
+
+namespace GranjaSystemProject.Tests.Tests.TestInfrastructure.Builders;
+
+public class UserBuilder
+{
+    private readonly User _user = new();
+
+    public UserBuilder()
+    {
+        _user.Name = "Usuário Teste";
+        _user.Email = $"user_{Guid.NewGuid()}@hotmail.com";
+        _user.PasswordHash = TestConstants.ValidPassword;
+        _user.BirthDate = new DateTime(1990, 1, 1);
+        _user.Type = UserType.Administrador;
+    }
+
+    public UserBuilder WithName(string name)
+    {
+        _user.Name = name;
+        return this;
+    }
+
+    public UserBuilder WithEmail(string email)
+    {
+        _user.Email = email;
+        return this;
+    }
+
+    public UserBuilder WithPassword(string password)
+    {
+        _user.PasswordHash = password;
+        return this;
+    }
+
+    public UserBuilder WithType(UserType type)
+    {
+        _user.Type = type;
+        return this;
+    }
+
+    public UserBuilder WithBirthDate(DateTime birthDate)
+    {
+        _user.BirthDate = birthDate;
+        return this;
+    }
+
+    public UserBuilder WithHashedPassword()
+    {
+        _user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(_user.PasswordHash);
+        return this;
+    }
+
+    public User Build() => _user;
+}
